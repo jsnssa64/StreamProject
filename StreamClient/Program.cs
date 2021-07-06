8efa817fda64fd183e4b2b24d26192002821abd0
JSDance <jsnsousa92@hotmail.com>
2021-07-06 18:29:26 +0100

    Update Project Naming

---
+++ StreamClient/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StreamClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
